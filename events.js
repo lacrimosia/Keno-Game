f55a1dd81dd32538fb94e11c46839cc9a42cd9eb
Shatilla Prayer <prayers@unlv.nevada.edu>
2016-02-03 16:04:38 -0800

    Changed code

--- events.js
+++ events.js
@@ -1,59 +1,52 @@
-// all events for the game are in this file
-
-$(function(){
-
-// our game instance
-var game = new keno.game(1);
-
-// init
-$("#denom").text(game.up);
-$("#max").text(game.maxBet);
-$("#total").text(game.total);
-
-
-// testing our game values
-game.Values();
-
-$('.nums').click(function(){
-	var that = this;
-	game.number++;
-
-	if(game.number <=7){
-		// dynamic pay changes
-		game.pays(game.number);
-		game.addChosenNum(this.id, game.number);
-		$(that).toggleClass("chosenText");
-		$("#chosen").html(game.newNum.join(", "));
-		// console.log("number", game.number);
-	}
-
-	if(!$(that).hasClass("chosenText")){
-		game.removeChosenNum(this.id);
-	}
-});
-
-$('#up').click(function(){
-	$("#denom").text(game.goUp());
-	$("#total").text(game.upBet());
-});
-
-$('#down').click(function(){
-	$("#denom").text(game.goDown());
-	$("#total").text(game.downBet());
-});
-
-$('#max').click(function(){
-	$("#denom").text(game.maxBet);
-	$("#total").text(game.maxed());
-});
-
-$("#clear").click(function(){
-
-});
-
-$("#spin").click(function(){
-  // winning numbers
-   $("#winner").append(game.winningNumbers());
-});
-
+// all events and calls are in this file
+$(function() {
+  // our game instance
+  var game = new keno.game(1);
+  // init Values
+  $("#denom").text(game.up);
+  $("#max").text(game.maxBet);
+  $("#total").text(game.total);
+  // load all the keno numbers init
+   game.Values(0,81,'#numbers');
+  $('.nums').click(function() {
+    var that = this;
+    game.number++;
+    if (game.number <= 7) {
+      // dynamic pay changes
+      game.pays(game.number);
+      game.addChosenNum(this.id, game.number);
+      $(that).toggleClass("chosenText");
+      $("#chosen").html(game.newNum.join(", "));
+      // console.log("number", game.number);
+    }
+    if (!$(that).hasClass("chosenText")) {
+      game.removeChosenNum(this.id);
+    }
+  });
+  $('#up').click(function() {
+    $("#denom").text(game.goUp());
+    $("#total").text(game.upBet());
+  });
+  $('#down').click(function() {
+    $("#denom").text(game.goDown());
+    $("#total").text(game.downBet());
+  });
+  $('#max').click(function() {
+    $("#denom").text(game.maxBet);
+    $("#total").text(game.maxed());
+  });
+  $("#clear").click(function() {
+  });
+	// play button
+  $("#spin").click(function() {
+    // winning numbers
+    $("#winner").append(game.winningNumbers());
+  });
+  // show our jackpot
+  var theJackpot = setInterval(function() {
+    times()
+  }, 500);
+  var times = function() {
+    $('.jacks').text("$" + game.jackpot++);
+  }
 });
--- keno_game.js
+++ keno_game.js
@@ -2,79 +2,70 @@
 Developed By Shatilla Prayer
 Copyright 2014-2015
 */
-
 // This is the main file that handles all the functionality of the game
 // This is where game features will appear
 
 var keno = keno || {};
-
 // game constructor
 keno.game = function(up) {
   this.up = up; //$1 bet
-  this.numMax = 80;     // maximum numbers
-  this.maxBet = 30;     //max bet of $30
-  this.total = 1000;    //total amount of casino cash
-  this.kenoNum = [];    //array of all keno numbers
-  this.ranNum = [];     //random generated array
-  this.newNum = [];     //chosen numbers that user picks
-  this.randomPicks = [];   //random picks will be stored here
-  this.win = [];         //winning keno numbers
-  this.count = 0;        //counts the number of times user picks a number
+  this.numMax = 80; // maximum numbers
+  this.maxBet = 30; //max bet of $30
+  this.total = 1000; //total amount of casino cash
+  this.kenoNum = []; //array of all keno numbers
+  this.ranNum = []; //random generated array
+  this.newNum = []; //chosen numbers that user picks
+  this.randomPicks = []; //random picks will be stored here
+  this.win = []; //winning keno numbers
+  this.count = 0; //counts the number of times user picks a number
   this.chosenBet = false; // chosen bet
   this.number = 0;
   this.jackpot = 12000;
   this.sevenMatch = this.jackpot; // jackpot amount
 }
-
 //controls the up buttons
 keno.game.prototype.goUp = function() {
   if (this.up >= 0 && this.up < 30) {
     return this.up += 1;
   }
 };
-
 // controls the down button
 keno.game.prototype.goDown = function() {
   if (this.up > 0) {
     return this.up -= 1;
   }
 };
-
 // default values users will choose
-keno.game.prototype.Values = function() {
+// params - start, end, selector
+keno.game.prototype.Values = function(a,b,c) {
   //keno numbers pushed dynamically into array
-  for (var m = 0; m < 81; m++) {
+  for (var m = a; m < b; m++) {
     this.kenoNum.push(m); //this loop pushes the 80 numbers inside the array
     for (j = m; j < this.kenoNum.length; j++) {
       //this loop takes those numbers and inserts them into html divs for display
-      // console.log("j", j);
-      $('#numbers').append('<div class="nums ' + this.kenoNum[j] + '" id="' + this.kenoNum[j] + '" name="box">' + this.kenoNum[j] + '</div>');
-      var value = $('#' + this.kenoNum[j]).html();
+    //  console.log("j", j);
+    $(c).append('<div class="nums ' + this.kenoNum[j] + '" id="' + this.kenoNum[j] + '" name="box">' + this.kenoNum[j] + '</div>');
+     var value = $('#' + this.kenoNum[j]).html();
     }
   }
 };
-
 // Max Bet
 keno.game.prototype.maxed = function() {
   var newTotal = this.total - this.maxBet;
   return newTotal;
 };
-
 // Increasing the Bet
 keno.game.prototype.upBet = function() {
   return this.total -= 1;
 };
-
 // Decreasing the Bet
 keno.game.prototype.downBet = function() {
   return this.total += 1;
 };
-
 // Reset the Bet
 keno.game.prototype.reset = function() {
   return this.up = 0;
 };
-
 // Add chosen numbers to array
 keno.game.prototype.addChosenNum = function(chosenNums, count) {
   if (count <= 7) {
@@ -84,26 +75,23 @@ keno.game.prototype.addChosenNum = function(chosenNums, count) {
     return this.newNum;
   }
 };
-
-
 // Output the pay amounts
 keno.game.prototype.pays = function(numAmount) {
   var gameNum = this.up;
   var pays = 1.00;
   var payAmounts = [];
-  for(var x=0; x<8; x++){
-  //  $("#p"+x).html("$" + matches);
+  for (var x = 0; x < 8; x++) {
+    //  $("#p"+x).html("$" + matches);
 
-   if(numAmount == x){
-     var nums = numAmount *= 5;
-     var matches = gameNum * (pays * nums);
-     payAmounts.push(matches*x);
-   }
+    if (numAmount == x) {
+      var nums = numAmount *= 5;
+      var matches = gameNum * (pays * nums);
+      payAmounts.push(matches * x);
+    }
   }
   console.log('pay Amounts', payAmounts);
   return payAmounts;
 };
-
 // Remove Chosen numbers from array
 keno.game.prototype.removeChosenNum = function(chosenNums) {
   for (var x = 0; x < this.newNum.length; x++) {
@@ -115,11 +103,10 @@ keno.game.prototype.removeChosenNum = function(chosenNums) {
     return getNums;
   }
 };
-
 // get the winning numbers to compare to user numbers
 keno.game.prototype.winningNumbers = function() {
   for (var i = 0; i < 8; i++) {
-       this.win.push(Math.floor((Math.random() * this.numMax) + i));
+    this.win.push(Math.floor((Math.random() * this.numMax) + i));
   }
   // return entire array with random values for winning numbers
   console.log(this.win);
